2fa79e1d8a132d27710f41224668118bd2d4e16b
mkzheng972 <markzheng27@gmail.com>
2020-04-04 20:13:27 -0400

    fixed admin chat/update buttons

--- client/components/Users/SingleUser.js
+++ client/components/Users/SingleUser.js
@@ -23,33 +23,37 @@ const SingleUser = props => {
       <div className="allJobs">
         <div key={user._id}>
           <img src={user.image} />
-          <h4>{singleUser.firstName}</h4>
+          <h4>
+            {singleUser.firstName} {singleUser.lastName}
+          </h4>
           <p>{singleUser.email}</p>
 
-          <div>
-            <NewChannel />
-          </div>
-          {user._id === singleUser._id && (
-            <p>
-              {' '}
-              <Link to={`/myprofile/${id}/update`}>
-                <Button variant="contained" color="primary">
-                  Update My Profile
-                </Button>
-              </Link>{' '}
-            </p>
-          )}
-
-          {user.isAdmin && (
-            <Link to={`/users/${singleUser._id}/update`}>
-              <Button variant="contained" color="primary">
-                Update This User
-              </Button>
-            </Link>
-          )}
-
-          <div>
-            <img src={user.imageUrl} />
+          <div className="user-profile-buttons">
+            {singleUser._id === user._id ? null : (
+              <div>
+                <NewChannel />
+              </div>
+            )}
+            {user._id === singleUser._id ? (
+              <div id="update-profile-button">
+                <p>
+                  <Link to={`/myprofile/${id}/update`}>
+                    <Button variant="contained" color="primary">
+                      Update My Profile
+                    </Button>
+                  </Link>
+                </p>
+              </div>
+            ) : null}
+            {user.isAdmin && user._id !== singleUser._id ? (
+              <div>
+                <Link to={`/users/${singleUser._id}/update`}>
+                  <Button variant="contained" color="primary">
+                    Update This User
+                  </Button>
+                </Link>
+              </div>
+            ) : null}
           </div>
         </div>
       </div>
